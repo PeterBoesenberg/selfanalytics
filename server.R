aea0c0a29e375fff828cbf289a45d15be0916a90
Peter Bösenberg <peter.boesenberg@functionhr.de>
2020-10-01 09:26:14 +0200

    first chart, maybe artifacts

--- server.R
+++ server.R
@@ -4,13 +4,23 @@ library(plotly)
 source("R/Profile.R")
 
 shinyServer(function(input, output) {
-  profile <- Profile$new()
-  profile$read()
-  print(profile$name)
-  print(profile$shares)
-  fwrite(profile$shares, "profile.csv")
+  # profile <- Profile$new()
+  # profile$read()
+  # print(profile$name)
+  # print(profile$shares)
+  # fwrite(profile$shares, "profile.csv")
 
-  output$linkedin_performance <- renderPlotly(
-    plot_ly(type = "bar", orientation = "h", name = "female")
-  )
+  shares <- fread("profile.csv")
+  print(shares)
+  shares <- shares[, id:=nrow(shares):1]
+  shares <- shares[is.na(comments), comments := 0]
+  
+  # shares <- shares[]
+  output$linkedin_performance <- renderPlotly({
+    plot <- plot_ly(shares, mode = 'lines')
+    plot <- add_trace(plot, x = ~id, y = ~likes, name = "Likes")
+    plot <- add_trace(plot, x = ~id, y = ~comments, name = "Comments")
+    plot <- add_trace(plot, x = ~id, y = ~views, name = "Views")
+    plot
+  })
 })
