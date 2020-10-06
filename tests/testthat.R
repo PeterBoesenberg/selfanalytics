40ff82927220c7a45ca2b308cc8aaf540744a81c
Peter Bösenberg <peter.boesenberg@functionhr.de>
2020-10-06 12:06:42 +0200

    de-packaged, tests, bitbucket

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,1 @@
 library(testthat)
-library(selfanalytics)
-
-test_check("selfanalytics")
--- tests/testthat/test_linting.R
+++ tests/testthat/test_linting.R
@@ -1,4 +1,4 @@
 test_that("linting", {
-  lint_result <- lintr::lint_package()
+  lint_result <- lintr::lint_dir("../..")
   expect_equal(length(lint_result), 0)
 })
